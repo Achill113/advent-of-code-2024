219930b183272ca4a06986cb69fd132a37fc4f68
Alex Hill <achill113@gmail.com>
2024-12-02 19:47:53 -0500

    day-1 part1 passing

--- day-01/src/part1.rs
+++ day-01/src/part1.rs
@@ -1,6 +1,45 @@
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+    let mut diff: i32 = 0;
+    let mut left_location_ids: Vec<i32> = Vec::<i32>::new();
+    let mut right_location_ids: Vec<i32> = Vec::<i32>::new();
+    let rows: Vec<&str> = _input.lines().collect();
+
+    for row in rows.into_iter() {
+        let location_ids: Vec<&str> = row.split("   ").collect();
+
+        let left_str = location_ids[0];
+        let right_str = location_ids[1];
+
+        match left_str.parse::<i32>() {
+            Ok(left) => {
+                match right_str.parse::<i32>() {
+                    Ok(right) => {
+                        left_location_ids.push(left);
+                        right_location_ids.push(right);
+                    },
+                    Err(e) => panic!("Failed to parse int: {} ({right_str})", e)
+                }
+            },
+            Err(e) => panic!("Failed to parse int: {} ({left_str})", e)
+        }
+    }
+
+    left_location_ids.sort();
+    right_location_ids.sort();
+
+    for i in 0..left_location_ids.len() {
+        let left = left_location_ids[i];
+        let right = right_location_ids[i];
+
+        println!("{left} - {right}");
+
+        diff += (left - right).abs();
+    }
+
+    println!("Result: {diff}");
+
+    Ok(diff.to_string())
 }
 
 #[cfg(test)]
