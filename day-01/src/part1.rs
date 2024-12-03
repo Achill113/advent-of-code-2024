79cff349a3f3cce9ed3dc6b8bd6afa8bfcb7a20d
Alex Hill <achill113@gmail.com>
2024-12-02 19:09:00 -0500

    day-1 template

---
+++ day-01/src/part1.rs
@@ -0,0 +1,21 @@
+#[tracing::instrument]
+pub fn process(_input: &str) -> miette::Result<String> {
+    todo!("day 01 - part 1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("11", process(input)?);
+        Ok(())
+    }
+}
