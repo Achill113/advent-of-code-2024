ae0c84ab86f42be4d94a8d6a8ee2eca459d975ba
Alex Hill <achill113@gmail.com>
2024-12-02 21:04:35 -0500

    day-02 part1 complete

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,6 +1,45 @@
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+    let mut result = 0;
+    for (index, report) in _input.lines().enumerate() {
+        println!("Report: {index}");
+
+        let levels = report.split_whitespace().map(|x| x.parse::<i32>().unwrap());
+        let mut last_level = -1;
+        let mut direction = 0;
+        let mut safe = true;
+
+        for level in levels {
+            if last_level != -1 {
+                if direction == 0 {
+                    if level > last_level {
+                        direction = 1;
+                    } else {
+                        direction = -1;
+                    }
+                }
+
+                let diff = level - last_level;
+
+                println!("Direction: {direction}, Last Level: {last_level}, Level: {level}, Diff: {diff}");
+
+                if direction > 0 && (diff < 1 || diff > 3) {
+                    safe = false;
+                } else if direction < 0 && (diff < -3 || diff > -1) {
+                    safe = false;
+                }
+            }
+
+            last_level = level;
+        }
+
+        println!("Safe: {safe}");
+        if safe {
+            result += 1;
+        }
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +48,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
