63eac003a25f0c17c73d1f19435337271d07b6a3
Alex Hill <achill113@gmail.com>
2024-12-03 14:35:05 -0500

    day-03 complete

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,6 +1,23 @@
+use regex::Regex;
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+    let mut result: i32 = 0;
+    let pattern = r"mul\(\d{1,3},\d{1,3}\)";
+    let regexer = Regex::new(pattern).unwrap();
+
+    for command_match in regexer.find_iter(_input) {
+        let command = command_match.as_str();
+
+        let first_index = command.find('(').unwrap(); // we know this should always return a value
+        let last_index = command.find(')').unwrap(); // we know this should always return a vlue
+        let num_substr = &command[first_index + 1..last_index];
+        let nums: Vec<i32> = num_substr.split(',').map(|x| x.parse::<i32>().unwrap()).collect();
+
+        result += nums[0] * nums[1];
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +26,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!("161", process(input)?);
         Ok(())
     }
 }
--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,6 +1,35 @@
-#[tracing::instrument]
+use regex::Regex;
+use tracing::instrument;
+
+#[instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    let mut result: i32 = 0;
+    let mut act = true;
+    let pattern = r"(mul\(\d{1,3},\d{1,3}\))|(do(n't)?\(\))";
+    let regexer = Regex::new(pattern).unwrap();
+
+    for command_match in regexer.find_iter(_input) {
+        let command = command_match.as_str();
+
+        if command == "don't()" {
+            act = false;
+            continue;
+        } else if command == "do()" {
+            act = true;
+            continue;
+        }
+
+        if act {
+            let first_index = command.find('(').unwrap(); // we know this should always return a value
+            let last_index = command.find(')').unwrap(); // we know this should always return a vlue
+            let num_substr = &command[first_index + 1..last_index];
+            let nums: Vec<i32> = num_substr.split(',').map(|x| x.parse::<i32>().unwrap()).collect();
+
+            result += nums[0] * nums[1];
+        }
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +38,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!("48", process(input)?);
         Ok(())
     }
 }
